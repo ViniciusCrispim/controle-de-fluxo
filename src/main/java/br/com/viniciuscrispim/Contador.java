027874884d02f002e3beff7d247afe49d951cc5e
Vinícius Crispim <viniciuswoliveira@outlook.com>
2025-06-24 14:41:07 -0300

    Feature: Continuidade do programa até que seja inserido os parametros corretos. while

--- src/main/java/br/com/viniciuscrispim/Contador.java
+++ src/main/java/br/com/viniciuscrispim/Contador.java
@@ -7,15 +7,20 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         var input = new Scanner(System.in);
-        try{
-            System.out.println("Digite o primeiro parâmetro");
-            var parametroUm = input.nextInt();
-            System.out.println("Digite o segundo parâmetro");
-            var parametroDois = input.nextInt();
+        var terminate = false;
 
-            contar(parametroUm, parametroDois);
-        } catch (ParametrosInvalidosException e) {
-            System.out.println("O segundo parâmetro precisa ser maior que o primeiro.");
+        while(!terminate) {
+            try{
+                System.out.println("Digite o primeiro parâmetro");
+                var parametroUm = input.nextInt();
+                System.out.println("Digite o segundo parâmetro");
+                var parametroDois = input.nextInt();
+
+                contar(parametroUm, parametroDois);
+                terminate = true;
+            } catch (ParametrosInvalidosException e) {
+                System.out.println("\nO segundo parâmetro deve ser maior que o primeiro");
+            }
         }
 
     }
@@ -26,6 +31,7 @@ public class Contador {
         }
 
         int contagem = parametroDois - parametroUm;
+        System.out.printf("\nIniciando a contagem:\n");
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
